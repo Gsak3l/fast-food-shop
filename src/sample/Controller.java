5460ec360e7a68ce43c41d795294f21f9de1d426
gsak3l <gsakel5@gmail.com>
2019-11-25 21:10:01 +0200

    so many things are working

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,28 +2,49 @@ package sample;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
 
 public class Controller {
 
     @FXML
     private ResourceBundle resources;
-
+    @FXML
+    private Spinner posotitaF;
+    @FXML
+    private Spinner posotitaP;
+    @FXML
+    ObservableList<String> dishes = FXCollections.observableArrayList(
+            "Γύρος", "Σουβλάκι", "Σουτζουκάκι", "Χωριάτικη", "Γεμιστά");
+    @FXML
+    ObservableList<String> drinks = FXCollections.observableArrayList(
+            "Πορτοκαλάδα", "Κόκα Κόλα", "Λεμονάδα", "Νερό", "Βότκα");
     @FXML
     private URL location;
-
     @FXML
-    private Text epilogiFagitouLabel;
-
+    private ComboBox drinksComboBox;
     @FXML
     private ListView foodList;
 
     @FXML
+    void printValue(MouseEvent event) {
+        System.out.println(posotitaF.getValue());
+    }
+
+    @FXML
     void initialize() {
-        assert epilogiFagitouLabel != null : "fx:id=\"epilogiFagitouLabel\" was not injected: check your FXML file 'sample.fxml'.";
         assert foodList != null : "fx:id=\"foodList\" was not injected: check your FXML file 'sample.fxml'.";
-        foodList.getItems().addAll("hello", "its me", "i was wondering", "if you want", "to marry me");
+        foodList.getItems().addAll(dishes);
+        posotitaF.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 100));
+        drinksComboBox.getItems().addAll(drinks);
+        posotitaP.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 100));
     }
 }
