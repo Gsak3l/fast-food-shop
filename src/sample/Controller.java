17f1ab9cec7a0a5b953add879eca32ed725bcfe7
gsak3l <gsakel5@gmail.com>
2019-11-26 13:20:31 +0200

    some comments added

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -14,7 +14,10 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
 
 public class Controller {
-
+    private double[] foodCost = new double[6];
+    private double[] drinkCost = new double[4];
+    private Image[] availableFoodImages = new Image[6];
+    private Image[] availableDrinksImages = new Image[4];
     @FXML
     private ResourceBundle resources;
     @FXML
@@ -27,8 +30,6 @@ public class Controller {
     @FXML
     ObservableList<String> drinks = FXCollections.observableArrayList(
             "Κόκα Κόλα", "Πορτοκαλάδα", "Λεμονάδα", "Νερό");
-    private Image[] availableFoodImages = new Image[6];
-    private Image[] availableDrinksImages = new Image[4];
     @FXML
     private URL location;
     @FXML
@@ -47,6 +48,8 @@ public class Controller {
 
     @FXML
     void showImage(MouseEvent event) {
+        //onclick action for the listview
+        //show image on click event
         if (foodList.getSelectionModel().getSelectedItem().equals("Γύρος"))
             foodImage.setImage(availableFoodImages[0]);
         else if (foodList.getSelectionModel().getSelectedItem().equals("Σουβλάκι"))
@@ -63,22 +66,29 @@ public class Controller {
 
     @FXML
     void showDrinks(ActionEvent event) {
-        if(drinksComboBox.getSelectionModel().getSelectedItem().equals("Κόκα Κόλα"))
+        //on action event for the combobox
+        //show image on action event
+        if (drinksComboBox.getSelectionModel().getSelectedItem().equals("Κόκα Κόλα"))
             drinksImage.setImage(availableDrinksImages[0]);
-        else if(drinksComboBox.getSelectionModel().getSelectedItem().equals("Πορτοκαλάδα"))
+        else if (drinksComboBox.getSelectionModel().getSelectedItem().equals("Πορτοκαλάδα"))
             drinksImage.setImage(availableDrinksImages[1]);
-        else  if(drinksComboBox.getSelectionModel().getSelectedItem().equals("Λεμονάδα"))
+        else if (drinksComboBox.getSelectionModel().getSelectedItem().equals("Λεμονάδα"))
             drinksImage.setImage(availableDrinksImages[2]);
-        else if(drinksComboBox.getSelectionModel().getSelectedItem().equals("Νερό"))
+        else if (drinksComboBox.getSelectionModel().getSelectedItem().equals("Νερό"))
             drinksImage.setImage(availableDrinksImages[3]);
     }
 
     @FXML
     void initialize() {
+        //adding the available dishes to the listView
         foodList.getItems().addAll(dishes);
+        //adding the available spinner values for the dishes
         posotitaF.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 100));
+        //adding the available drinks to the drink comboBox
         drinksComboBox.getItems().addAll(drinks);
+        //adding the available spinner values for the drinks
         posotitaP.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 100));
+        //setting images for food and drinks
         availableFoodImages[0] = new Image("sample/gyros.jpg");
         availableFoodImages[1] = new Image("sample/souvlaki.jpg");
         availableFoodImages[2] = new Image("sample/soutsoukaki.jpg");
@@ -89,5 +99,16 @@ public class Controller {
         availableDrinksImages[1] = new Image("sample/portokalada.jpg");
         availableDrinksImages[2] = new Image("sample/lemonada.jpg");
         availableDrinksImages[3] = new Image("sample/nero.jpg");
+        //initializing cost for food and drinks
+        foodCost[0] = 3.20;
+        foodCost[1] = 3.00;
+        foodCost[2] = 3.30;
+        foodCost[3] = 5.00;
+        foodCost[4] = 4.20;
+        foodCost[5] = 4.00;
+        drinkCost[0] = 1.20;
+        drinkCost[1] = 1.00;
+        drinkCost[2] = 1.00;
+        drinkCost[3] = 0.50;
     }
 }
